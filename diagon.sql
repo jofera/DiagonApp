7a5cd2c55f52348fa1a10c1389b5f3dec4d9ee64
Gonzalo Fernandez <jofera@gmail.com>
2014-06-26 18:29:28 +0200

    Cambios en la base de datos

--- diagon.sql
+++ diagon.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-06-2014 a las 18:21:11
+-- Tiempo de generación: 26-06-2014 a las 18:26:48
 -- Versión del servidor: 5.5.36
 -- Versión de PHP: 5.4.27
 
@@ -56,7 +56,26 @@ CREATE TABLE IF NOT EXISTS `cita` (
   PRIMARY KEY (`id`),
   KEY `id_medico` (`id_medico`,`id_usuario`),
   KEY `id_usuario` (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=14 ;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`id`, `id_medico`, `id_usuario`, `consulta`, `fecha`, `hora`, `estado`) VALUES
+(1, 7, 10, '1', '2014-06-19', '17:46:54', 0),
+(2, 7, 8, '11', '2014-06-20', '17:45:27', 0),
+(3, 7, 10, '22', '2014-06-18', '17:45:27', 0),
+(4, 6, 10, '23', '2014-06-20', '17:45:27', 0),
+(5, 6, 10, '23', '2014-06-23', '17:45:27', 0),
+(6, 6, 10, '23', '2014-06-19', '19:59:29', 2),
+(7, 6, 10, '23', '2014-06-19', '19:59:29', 3),
+(8, 6, 10, '23', '2014-06-18', '19:44:05', 2),
+(9, 6, 10, '23', '2014-06-23', '19:44:05', 3),
+(10, 7, 9, '1', '2014-06-23', '17:45:27', 3),
+(11, 7, 10, '22', '2014-06-23', '17:45:27', 3),
+(12, 6, 10, '23', '2014-06-25', '14:08:06', 2),
+(13, 6, 10, '23', '2014-06-26', '18:02:19', 1);
 
 -- --------------------------------------------------------
 
@@ -172,7 +191,14 @@ CREATE TABLE IF NOT EXISTS `tratamiento` (
   PRIMARY KEY (`id`),
   KEY `id_medico` (`id_medico`,`id_paciente`),
   KEY `id_usuario` (`id_paciente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `tratamiento`
+--
+
+INSERT INTO `tratamiento` (`id`, `id_medico`, `id_paciente`, `fecha_inicio`, `duracion`, `descripcion`) VALUES
+(1, 6, 1, '2014-06-19', 2, 'dasdasdas');
 
 -- --------------------------------------------------------
 
@@ -197,9 +223,9 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `dni`, `password`, `nombre`, `apellidos`, `direccion`, `telefono`, `fecha_nacimiento`) VALUES
-(8, '44332233F', '1234', 'Marta', 'Lopez', 'C/Tupu', '675432345', '2000-06-02'),
+(8, '44332233F', '1234', 'Marta', 'Lopez', 'C/Tupu', '675432345', '2000-06-01'),
 (9, '44556677K', '1234', 'Pepe', 'Ruiz', 'c/DiputaProject', '657543456', '1990-06-02'),
-(10, '1', '1', 'José', 'Moles García', 'C/ De prueba, 3', '951888888', '2014-06-10'),
+(10, '1', '1', 'Jose', 'Moles Garcia', 'C/ De prueba, 3', '951888888', '2014-06-01'),
 (11, '2', '2', 'Francisco', 'García Fernández', 'C/ Dirección de prueba, 8', '951888888', '2014-06-01'),
 (12, 'admin', 'admin', 'admin', 'admin', NULL, NULL, NULL);
 
@@ -247,10 +273,11 @@ ALTER TABLE `roles`
 -- Filtros para la tabla `tratamiento`
 --
 ALTER TABLE `tratamiento`
-  ADD CONSTRAINT `tratamiento_ibfk_2` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id`),
   ADD CONSTRAINT `FK_tratamiento_ibfj` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id`),
   ADD CONSTRAINT `FK_tratamiento_id_medico` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id`),
-  ADD CONSTRAINT `tratamiento_ibfk_1` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id`);
+  ADD CONSTRAINT `FK_tratamiento_id_paciente` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id`),
+  ADD CONSTRAINT `tratamiento_ibfk_1` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id`),
+  ADD CONSTRAINT `tratamiento_ibfk_2` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
